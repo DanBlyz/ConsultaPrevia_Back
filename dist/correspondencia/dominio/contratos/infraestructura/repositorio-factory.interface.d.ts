35825db199eff1345cec17e6b0fc689d452dee58
DanBlyz <71474198+DanBlyz@users.noreply.github.com>
2023-07-11 16:41:24 -0400

    Add files via upload

---
+++ dist/correspondencia/dominio/contratos/infraestructura/repositorio-factory.interface.d.ts
@@ -0,0 +1,36 @@
+import { QueryRunner } from 'typeorm';
+import { IAdjuntoRepositorio, IBuzonRepositorio, IBuzonUsuarioRepositorio, IClasificacionBuzonRepositorio, IClasificacionRepositorio, IContactoRepositorio, IContenidoDetalleRepositorio, IContenidoRepositorio, IDocumentoDetalleRepositorio, IDocumentoRepositorio, IDocumentoSeguimientoRepositorio, IEtiquetaRepositorio, IHojaRutaRepositorio, INotaRepositorio, IParticipanteRepositorio, IPersonaRepositorio, IPlantillaRepositorio, IPuestoRepositorio, ISeguimientoRepositorio, ITipoDocumentoRepositorio, IUniOrganizacionalRepositorio, IGrupoRepositorio, IRolRepositorio, ICuentaRepositorio, IRolCuentaRepositorio, IUsuarioRepositorio, IPruebaRepositorio } from './repositorios';
+export declare const REPOSITORIO_FACTORY = "REPOSITORIO_FACTORY";
+export interface IRepositorioFactory {
+    adjuntoRepositorio: IAdjuntoRepositorio;
+    buzonUsuarioRepositorio: IBuzonUsuarioRepositorio;
+    buzonRepositorio: IBuzonRepositorio;
+    clasificacionBuzonRepositorio: IClasificacionBuzonRepositorio;
+    clasificacionRepositorio: IClasificacionRepositorio;
+    contactoRepositorio: IContactoRepositorio;
+    contenidoDetalleRepositorio: IContenidoDetalleRepositorio;
+    contenidoRepositorio: IContenidoRepositorio;
+    documentoDetalleRepositorio: IDocumentoDetalleRepositorio;
+    documentoSeguimientoRepositorio: IDocumentoSeguimientoRepositorio;
+    documentoRepositorio: IDocumentoRepositorio;
+    etiquetaRepositorio: IEtiquetaRepositorio;
+    hojaRutaRepositorio: IHojaRutaRepositorio;
+    notaRepositorio: INotaRepositorio;
+    participanteRepositorio: IParticipanteRepositorio;
+    personaRepositorio: IPersonaRepositorio;
+    plantillaRepositorio: IPlantillaRepositorio;
+    puestoRepositorio: IPuestoRepositorio;
+    seguimientoRepositorio: ISeguimientoRepositorio;
+    tipoDocumentoRepositorio: ITipoDocumentoRepositorio;
+    uniOrganizacionalRepositorio: IUniOrganizacionalRepositorio;
+    grupoRepositorio: IGrupoRepositorio;
+    rolRepositorio: IRolRepositorio;
+    cuentaRepositorio: ICuentaRepositorio;
+    rolCuentaRepositorio: IRolCuentaRepositorio;
+    usuarioRepositorio: IUsuarioRepositorio;
+    pruebaRepositorio: IPruebaRepositorio;
+    iniciarTransaccion(): Promise<QueryRunner>;
+    confirmar(transaccion: QueryRunner): Promise<void>;
+    revertir(transaccion: QueryRunner): Promise<void>;
+    liberar(transaccion: QueryRunner): Promise<void>;
+}
